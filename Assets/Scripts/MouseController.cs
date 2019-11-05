2aa7b1b8b721c69e3bbe63d38090f25d11d1a13e
Paul Leelaviwatana <apaulture@gmail.com>
2019-11-04 17:57:44 -0800

    - Added scene, scripts, and objects for a rock that you can click and drag - While dragging, the bar fills up

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(MeshCollider))]
+
+public class MouseController : MonoBehaviour
+{
+    float happiness;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    Vector3 curPosition;
+    Renderer renderer;
+    public Slider slider;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        
+    }
+
+    void OnMouseDown()
+    {
+        
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+
+        if (offset.magnitude > 0)
+        {
+            renderer.material.SetColor("_Color", Color.red);
+            slider.value += 0.01f;
+        }
+
+    }
+
+    private void OnMouseUp()
+    {
+        renderer.material.SetColor("_Color", Color.white);
+    }
+
+    private void Update()
+    {
+        // print(offset.magnitude);
+        print(slider.value);
+        
+    }
+
+}
